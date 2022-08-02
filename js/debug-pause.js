ef6a30016391dd360d491ee596353395805b46a3
DantonMariano <danton@zoocha.com>
2022-08-02 16:25:18 -0300

    Initial commit

---
+++ js/debug-pause.js
@@ -0,0 +1,46 @@
+(function ($, Drupal) {
+  Drupal.behaviors.debugpause = {
+    attach: (context, settings) => {
+      once('debugpause', 'html', context).forEach( function (element) {
+        let $toolbarItem = $('.toolbar-icon.toolbar-icon-debugpause-menu-link');
+        let isPausing = false;
+        let pausing;
+        let counting;
+
+        $('#debugpausebutton').click(()=>{
+          if (!isPausing) {
+            isPausing = true;
+            let seconds = $('#debugpausebutton').attr('pausein');
+            let secondsInt = parseInt(seconds/1000);
+            $toolbarItem.text(secondsInt);
+            $toolbarItem.prop('title', 'Cancels Pause');
+            pausing = setTimeout(()=>{
+              $toolbarItem.text('Paused');
+              debugger;
+              clearInterval(counting);
+              $toolbarItem.text('Debug Pause');
+              $toolbarItem.prop('title', 'Pauses Javascript');
+              isPausing = false;
+            },seconds)
+            counting = setInterval(()=>{
+              secondsInt--;
+              $toolbarItem.text(secondsInt);
+              if (secondsInt === 0) {
+                clearInterval(counting);
+              }
+            }, 1000);
+          } else {
+            clearTimeout(pausing);
+            clearInterval(counting);
+            $toolbarItem.text('Cancelled');
+            $toolbarItem.prop('title', 'Pauses Javascript');
+            setTimeout(()=>{
+              $toolbarItem.text('Debug Pause');
+              isPausing = false;
+            },2000)
+          }
+        })
+      })
+    }
+  }
+} (jQuery, Drupal))
---
+++ src/Form/debugpauseSettingsForm.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\debugpause\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class debugpauseSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'debugpause_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    // Form constructor
+    parent::buildForm($form, $form_state);
+    // Default settings
+    $config = $this->config('debugpause.settings');
+
+    $form['pausein'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Amount of time before it pauses'),
+      '#default_value' => $config->get('pausein'),
+      '#description' => $this->t('Amount of time in Miliseconds before it pauses.')
+    ];
+
+    $form['submit_button'] = [
+      '#type' => 'submit',
+      '#title' => $this->t('Submit'),
+      '#value' => $this->t('Submit')
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $config = $this->config('debugpause.settings');
+    $config->set('pausein', $form_state->getValue('pausein'));
+    $config->save();
+    return parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames()
+  {
+    return [
+      'debugpause.settings'
+    ];
+  }
+
+}
